eeb3f2ab1575fb5d64f3e70c69aabbb66ae3b7f1
jyrgenhallast <jurgen.hallast@khk.ee>
2017-12-08 09:28:24 +0200

    Kommentaaride kasutamise kirjeldus

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JHallast
+ * Date: 8.12.2017
+ * Time: 9:03
+ */
+// Üherealine kommentaar
+/* Mitme-
+ * realine
+ * kommentaar
+ * käib nii
+*/
+`
+?>
