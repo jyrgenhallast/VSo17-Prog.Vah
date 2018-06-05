c5ef82e64f3f7a0415e0702aadb472c58820fe52
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-06 02:10:42 +0300

    ++ ja -- operaatorid

--- numbrid.php
+++ numbrid.php
@@ -4,4 +4,23 @@ for($arv = 1; $arv <= 10; $arv++){
 		      $varv = 'blue';
 		 } else {
 		    $varv = 'red';}
-echo '<p style=color:'.$varv.'>'.$arv.'</style><br />';}
+echo '<p style=color:'.$varv.'>'.$arv.'</p><br>';
+}
+
+echo '<h3>++ ja -- </h3> <br> ';
+
+$a = 5; $b = 2;
+$c = $a++;
+$d = $b--;
+echo 'a = '.$a.'<br />';
+echo 'b = '.$b.'<br />';
+echo 'c = '.$c.'<br />';
+echo 'd = '.$d.'<br />';
+
+$a = 5; $b = 2;
+$c = ++$a;
+$d = --$b;
+echo 'a = '.$a.'<br />';
+echo 'b = '.$b.'<br />';
+echo 'c = '.$c.'<br />';
+echo 'd = '.$d.'<br />';
