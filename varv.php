340be9e547cabb98052ded0a01b0003cb0624e9d
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-06 01:26:13 +0300

    Teksti väljastamine värviliselt

--- varv.php
+++ varv.php
@@ -1,4 +1,13 @@
 <?php
-for($kord = 1; $kord <= 5; $kord++) {
-	echo 'See on tekst<br />';
-}
+for($rida = 1; $rida <= 5; $rida++){
+	    $varv = '#';
+	    for($kord = 1; $kord <= 6; $kord++){
+		       $juhuarv = rand(0, 15);
+		       $symbol = dechex($juhuarv);
+		       $varv = $varv.$symbol;
+		    }
+    echo '<h1 style=color:'.$varv.'>'.
+                  'Värviline tekst'.
+                  '</h1>'.
+                  '<br />';
+ }
