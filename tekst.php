8c9d718c917a12d0698dfe7b12d14d68232d049d
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-07 01:36:20 +0300

    Tekstitöötluse kirjeldus

---
+++ tekst.php
@@ -0,0 +1,25 @@
+<?php
+class tekst
+{ // klassi algus
+	// klassi omadused, väljad
+	// klassi muutuja
+	var $sone = ''; // teksti kirjeldavad sõnad
+	/**
+	 * tekst constructor.
+	 * @param string $sone
+	 */
+	public function __construct($sone)
+	{
+		$this->maaraTekst($sone);
+	}
+	// klassi tegevuse meetoditena
+	//teksti määramine
+	// maaraTekst(Tere Maailm!);
+	function maaraTekst($sone) {
+		$this->sone = $sone;
+	}
+	//teksti väljastamine
+	function prindiTekst () {
+		echo $this->sone.'<br/>';
+	}
+} // klassi lõpp
