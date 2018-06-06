2a27a1a600442d2c3096c8c33aee43a1e2ab2213
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-06 09:51:52 +0300

    Tabeli väljastamine

---
+++ funktsioonid.php
@@ -0,0 +1,14 @@
+<?php
+
+function tabel($sona1, $sona2, $sona3, $sona4){
+	    echo '<table border="1">';
+        for($rida = 1; $rida <= 4; $rida++){
+		            echo '<tr>';
+                echo '<td>';
+                    echo  ${'sona'}.$rida;
+                echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table>';
+}
+tabel('1', '2', '3', '4');
--- numbrid.php
+++ numbrid.php
@@ -24,3 +24,22 @@ echo 'a = '.$a.'<br />';
 echo 'b = '.$b.'<br />';
 echo 'c = '.$c.'<br />';
 echo 'd = '.$d.'<br />';
+
+
+echo '<h3>WHILE ja DO-WHILE </h3> ';
+
+$kord = 1;
+while($kord <= 5){
+	    if($kord == 3){
+		        break;
+    }
+    echo '<i>'.$kord++.'</i><br />';
+}
+
+$kord = 1;
+do {
+	    if($kord == 3){
+		        continue;
+    }
+    echo '<b>'.$kord.'</b><br />';
+} while(++$kord <= 5);
